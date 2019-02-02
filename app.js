08441b5638c5bdf1532ddf91272aa4038772a17c
elharony <p.yahyaelharony@gmail.com>
2019-02-02 09:22:44 +0200

    Add 3 buttons' functionality; bold, italic, blockquote

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ function init() {
 
     // Make the iframe's document "editable"
     editor.contentDocument.designMode = "on"
+
+    // "bold"
+    const opt_bold = document.querySelector("#make_bold")
+    opt_bold.addEventListener("click", () => {
+        editor.contentDocument.execCommand("bold", false, null)
+        editor.contentWindow.focus()
+    })
+
+    // "italic"
+    const opt_italic = document.querySelector("#make_italic")
+    opt_italic.addEventListener("click", () => {
+        editor.contentDocument.execCommand("italic", false, null)
+        editor.contentWindow.focus()
+    })
+
+    // "quote"
+    const opt_quote = document.querySelector("#make_quote")
+    opt_quote.addEventListener("click", () => {
+        editor.contentDocument.execCommand("formatBlock", false, "blockquote")
+        editor.contentWindow.focus()
+    })
+
 }
